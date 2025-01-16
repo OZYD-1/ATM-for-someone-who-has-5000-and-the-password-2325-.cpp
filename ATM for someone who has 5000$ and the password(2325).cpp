25598e8f0ea7b30316f39914e84f0df8d4ac0ef5
OZYD <zaedsmadi3@gmail.com>
2025-01-16 15:23:27 +0300

    Update ATM for someone who has 5000$ and the password(2325).cpp
    Signed-off-by: OZYD <zaedsmadi3@gmail.com>

--- ATM for someone who has 5000$ and the password(2325).cpp
+++ ATM for someone who has 5000$ and the password(2325).cpp
@@ -5,15 +5,16 @@ using namespace std;
 int passWord = 2325;
 double balance = 5000;
 int deposit = 0;
-int withdDraw = 0;
+int withDraw = 0;
 int choice1 = 0;
 char choice2 = 0;
+int Attempts = 5;
 
 //***the Menu of ATM***//
 void show() {
     cout << "\n\n***** Menu *****" << endl;
     cout << "1) balance" << endl;
-    cout << "2) withdDraw" << endl;
+    cout << "2) withDraw" << endl;
     cout << "3) deposit" << endl;
     cout << "4) Exit" << endl;
     cout << "****************\n" << endl;
@@ -27,25 +28,25 @@ void process() {
         show();
 
         do {
-            cout << "\nEnter your  choice : "; cin >> choice1;
+            cout << "\nEnter your choice : "; cin >> choice1;
             switch (choice1) {
             case 1:
-                cout << "\n\tYour balance is : " << balance << endl;
+                cout << "\n\tYour balance is : " << balance << " JD." << endl;
                 break;
             case 2:
-                cout << "\n\tEnter your Amount money : "; cin >> withdDraw;
+                cout << "\n\tEnter your Amount money : "; cin >> withDraw;
 
-                if (withdDraw < balance)
-                    balance -= withdDraw;
+                if (withDraw <= balance)
+                    balance -= withDraw;
                 else
                 {
-                    cout << "\nErorr : Sorry you can't withdDraw this Amount becouse your balance : " << balance << endl;
+                    cout << "\n|* Erorr : Sorry you can't withDraw this Amount becouse your balance : " << balance << " JD. *|" << endl;
                     break;
                 }
 
                 cout << "\n\t\tDo you want to know your current balance [Y OR N] ? : "; cin >> choice2;
                 if (choice2 == 'Y' || choice2 == 'y')
-                    cout << "\n\t\t\tYour balance is : " << balance;
+                    cout << "\n\t\t\t|* Your balance is : " << balance << " JD. *|" << endl ;
                 else;
 
                 break;
@@ -56,33 +57,39 @@ void process() {
                 cout << "\n\t\tDo you want to know your current balance [Y OR N] ? : "; cin >> choice2;
                 if (choice2 == 'Y' || choice2 == 'y')
                 {
-                    cout << "\n\t\t\tYour balance is : " << balance;
+                    cout << "\n\t\t\t|* Your balance is : " << balance << " JD. *|" << endl ;
                     break;
                 }
                 else
                     break;
             case 4:
-                cout << "\n\tThank You <3." << endl;
-                break;
+                cout << "\n\t|* Thank You <3. *|" << endl;
+                    break;
+            default:
+            cout<<"\n\t|* Your choice is't available try again *|"<<endl;
+            
             }
-        } while (choice1 < 4);
+        } while (choice1 != 4);
     }
     else
     {
         char option;
+        Attempts--;
         cout << "\n\tThe password is incorrect, Do you want to try again [Y or N] ? : "; cin >> option;
         if (option == 'Y' || option == 'y')
         {
-            cout << "\n\t\tEnter your passWord : "; cin >> passWord;
-            if (passWord == 2325)
+            if(Attempts!=0)
+            {
+                cout<< "\n\t\t\tYou have " << Attempts << " attempts left.\n" << endl;
                 process();
+            }
             else
-                cout << "\n\t\t\ttry again later <3." << endl;
+                cout << "\n\t\t\tYour attempts have been completed try again later <3." << endl;
         }
         else
         {
-            cout << "\n\t\tThank You <3." << endl;
-            choice1 = 4;
+            cout << "\n\t\t|* Thank You <3. *|" << endl;
+            
         }
     }
 }
